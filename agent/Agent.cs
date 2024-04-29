5804c2c94034ace10bcedfab82d76002ae463a75
Campione <80720402+alfrecampione@users.noreply.github.com>
2024-04-29 02:35:31 -0400

    Update Agent.cs

--- agent/Agent.cs
+++ agent/Agent.cs
@@ -1,263 +1,239 @@
-﻿using Agent.Enum;
+﻿using System.Security.Cryptography;
+using Agent.Enum;
 
 namespace Agent;
 
-class Tower((int, int) position, string name)
+readonly struct Troop(
+    (int, int) position,
+    int troops,
+    DesireState desire,
+    TerrainType terrain,
+    float height,
+    string name,
+    bool defenders)
 {
+    public (int, int) Position => position;
+    public int Troops => troops;
+    public DesireState Desire => desire;
+    public TerrainType Terrain => terrain;
+    public float Height => height;
     public string Name => name;
-    public (int, int) Position = position;
-    public bool OccupiedByDefender = true;
+    public bool Defenders => defenders;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Troop troop)
+            return false;
+        return troop.Position == Position;
+    }
 }
 
-abstract class Agent((int, int) position, int troops, DesireState desire, string name)
+readonly struct Tower((int, int) position, string name, bool defenders)
 {
     public string Name => name;
-    public int Troops => troops;
-    protected (int, int) Position = position;
+    public (int, int) Position => position;
+    public bool OccupiedByDefenders => defenders;
+}
 
-    private bool _followOrder = false;
+static class Agent
+{
+    private const int Range = 5;
+    private const float AttackProbability = 0.8f;
 
-    private List<(BeliefState, object?)> _beliefs = [];
-    private IntentionAvailable _available = IntentionAvailable.Available;
-    private IntentionHelp _needHelp = IntentionHelp.NoHelpNeeded;
-    private DesireState _desire = desire;
-    private (IntentionAction, object?) _intention = (IntentionAction.Move, null);
 
+    // A function to calculate the euclidean distance between two troops
+    private static float Distance(Troop troop1, Troop? troop2)
+    {
+        if (troop2 == null)
+            return 0;
+        return MathF.Sqrt(MathF.Pow(troop1.Position.Item1 - troop2.Value.Position.Item1, 2) +
+                          MathF.Pow(troop1.Position.Item2 - troop2.Value.Position.Item2, 2));
+    }
+
+    private static float Distance(Troop troop, Tower? tower)
+    {
+        if (tower == null)
+            return 0;
+        return MathF.Sqrt(MathF.Pow(troop.Position.Item1 - tower.Value.Position.Item1, 2) +
+                          MathF.Pow(troop.Position.Item2 - tower.Value.Position.Item2, 2));
+    }
+
+    private static bool IsAlly(Troop troop1, Troop troop2)
+    {
+        return troop1.Defenders && troop2.Defenders;
+    }
+
+    private static bool IsAlly(Troop troop, Tower tower2)
+    {
+        return troop.Defenders == tower2.OccupiedByDefenders;
+    }
+
+    public static (IntentionAction, object?) GetAction(Troop actualTroop, IEnumerable<Troop> troops,
+        IEnumerable<Tower> towers)
+    {
+        var beliefs = GetBeliefs(actualTroop, troops, towers).ToArray();
+        var intentions = GetIntention(actualTroop, beliefs);
+        throw new NotImplementedException();
+    }
 
     // Generating believes given the current state of the world
-    private void Brf(List<Agent> localAgents, List<Agent> globalAgents, List<Tower> localTowers,
-        List<Tower> globalTowers)
+    private static IEnumerable<(BeliefState, object?)> GetBeliefs(Troop actualTroop, IEnumerable<Troop> troops,
+        IEnumerable<Tower> towers)
     {
-        // Search for enemies
-        foreach (var agent in globalAgents.Where(agent => !IsAlly(agent)))
-        {
-            _beliefs.Add((BeliefState.EnemyOnSight, agent));
-        }
+        var beliefs = new List<(BeliefState, object?)>();
 
-        // Select the enemies that are in range
-        foreach (var agent in localAgents.Where(agent => !IsAlly(agent)))
+        // Identify allies and enemies
+        foreach (var troop in troops)
         {
-            _beliefs.Add((BeliefState.EnemyInRange, agent));
-        }
-
-        // Search for allies
-        foreach (var agent in globalAgents.Where(IsAlly))
-        {
-            _beliefs.Add((BeliefState.AlliesOnSight, agent));
-        }
-
-        // Select the allies that are in range
-        foreach (var agent in localAgents.Where(IsAlly))
-        {
-            _beliefs.Add((BeliefState.AlliesInRange, agent));
-        }
-
-        // Select the action to take for each enemy in range
-        foreach (var enemy in _beliefs.Where(b => b.Item1 == BeliefState.EnemyInRange).Select(b => b.Item2))
-        {
-            if (enemy is Agent enemyAgent)
-                _beliefs.Add(enemyAgent.Troops > Troops
-                    ? (BeliefState.PonderRetreat, enemyAgent)
-                    : (BeliefState.Attack, enemyAgent));
-        }
-
-        // Check for reinforcements
-        foreach (var enemy in _beliefs.Where(b => b.Item1 == BeliefState.PonderRetreat))
-        {
-            if (enemy.Item2 is not Agent enemyAgent) continue;
-            if (_beliefs.Any(b =>
-                    b.Item1 == BeliefState.AlliesInRange && (enemyAgent.Troops + Troops > enemyAgent.Troops) &&
-                    enemyAgent.IsAvailable()))
-                _beliefs.Add((BeliefState.AskForHelp, enemy.Item2));
-            else
-                _beliefs.Add((BeliefState.Retreat, enemy.Item2));
-        }
-
-        // Search for allies asking for help
-        foreach (var ally in localAgents.Where(IsAlly))
-        {
-            if (ally.NeedsHelp())
-                _beliefs.Add((BeliefState.ReinforcementOnWay, ally));
-        }
-
-        // Check for global enemy towers
-        foreach (var tower in globalTowers.Where(t => !IsAlly(t)))
-        {
-            _beliefs.Add((BeliefState.TowerOnSight, tower));
-        }
-
-        // Check for nerby enemy towers
-        foreach (var tower in localTowers.Where(t => !IsAlly(t)))
-        {
-            _beliefs.Add((BeliefState.TowerInRange, tower));
-        }
-
-        foreach (var tower in _beliefs.Where(b => b.Item1 == BeliefState.TowerInRange))
-        {
-            if (tower.Item2 is not Tower towerObj) continue;
-            if (_beliefs.All(b => b.Item1 != BeliefState.EnemyInRange))
-                _beliefs.Add((BeliefState.AttackTower, towerObj));
-        }
-
-        _beliefs.Add((BeliefState.Stay, null));
-        _beliefs.Add((BeliefState.Move, null));
-    }
-
-    private bool SearchAndSet(BeliefState belief, IntentionAction action)
-    {
-        var tempBeliefs = _beliefs.Where(b => belief == b.Item1);
-        var valueTuples = tempBeliefs.ToList();
-        if (valueTuples.Count == 0) return false;
-        {
-            _intention = (action, valueTuples.Select(b => b.Item2 as Agent).MinBy(a => Distance(this, a)));
-            return true;
-        }
-    }
-
-    // Filter the believes given the disires of the agent
-    private void Filter()
-    {
-        if (SearchAndSet(BeliefState.ReinforcementOnWay, IntentionAction.Help))
-            return;
-        if (SearchAndSet(BeliefState.Retreat, IntentionAction.Retreat))
-            return;
-        if (SearchAndSet(BeliefState.Attack, IntentionAction.Attack))
-            return;
-        if (SearchAndSet(BeliefState.AttackTower, IntentionAction.AttackTower))
-            return;
-        if (_desire == DesireState.GoAhead)
-        {
-            if (SearchAndSet(BeliefState.Move, IntentionAction.Move))
-                return;
-        }
-        else
-        {
-            if (SearchAndSet(BeliefState.Move, IntentionAction.StayClose))
+            if (IsAlly(actualTroop, troop))
             {
-                return;
+                beliefs.Add((BeliefState.AlliesOnSight, troop));
+                if (Distance(actualTroop, troop) < Range)
+                    beliefs.Add((BeliefState.AlliesInRange, troop));
+            }
+            else
+            {
+                beliefs.Add((BeliefState.EnemyOnSight, troop));
+                if (Distance(actualTroop, troop) < Range)
+                    beliefs.Add((BeliefState.EnemyInRange, troop));
             }
         }
 
-        if (SearchAndSet(BeliefState.Stay, IntentionAction.StayClose))
-            return;
-        throw new Exception("No intention found");
-    }
-
-    // A function to calculate the euclidean distance between two agents
-    private static double Distance(Agent agent1, Agent? agent2)
-    {
-        if (agent2 == null)
-            return 0;
-        return Math.Sqrt(Math.Pow(agent1.Position.Item1 - agent2.Position.Item1, 2) +
-                         Math.Pow(agent1.Position.Item2 - agent2.Position.Item2, 2));
-    }
-
-    // Execute the action given the filtered believes given the intentions of the agent
-    private void Execute()
-    {
-        _needHelp = _intention.Item1 switch
+        // Identify towers
+        foreach (var tower in towers)
         {
-            IntentionAction.Attack => IntentionHelp.NoHelpNeeded,
-            IntentionAction.AttackTower => IntentionHelp.NoHelpNeeded,
-            IntentionAction.Retreat => IntentionHelp.NoHelpNeeded,
-            IntentionAction.Help => IntentionHelp.NeedHelp,
-            IntentionAction.StayClose => IntentionHelp.NoHelpNeeded,
-            IntentionAction.Move => IntentionHelp.NoHelpNeeded,
-            IntentionAction.Stay => IntentionHelp.NoHelpNeeded,
-            _ => throw new ArgumentOutOfRangeException()
-        };
-        _available = IntentionAvailable.NotAvailable;
+            if (IsAlly(actualTroop, tower))
+            {
+                beliefs.Add((BeliefState.AllyTowerOnSight, tower));
+                if (Distance(actualTroop, tower) < Range)
+                    beliefs.Add((BeliefState.AllyTowerInRange, tower));
+            }
+            else
+            {
+                beliefs.Add((BeliefState.EnemyTowerOnSight, tower));
+                if (Distance(actualTroop, tower) < Range)
+                    beliefs.Add((BeliefState.EnemyTowerInRange, tower));
+            }
+        }
+
+
+        return beliefs;
     }
 
-    public void SetAvailable() => _available = IntentionAvailable.Available;
-
-    public bool IsAvailable() => _available == IntentionAvailable.Available;
-
-    public bool NeedsHelp() => _needHelp == IntentionHelp.NeedHelp;
-
-    public string Nlp(string message, List<Agent> localAgents, List<Agent> globalAgents,
-        List<Tower> localTowers, List<Tower> globalTowers)
+    private static (IntentionAction, object?) GetIntention(Troop actualTroop,
+        (BeliefState, object?)[] beliefs)
     {
-        _followOrder = true;
+        List<(IntentionAction, object?, float)> actions = [];
+
+        // Select each enemy tower on sight
+        var towers = beliefs.Where(b => b.Item1 is BeliefState.EnemyTowerOnSight)
+            .Select(b => b.Item2 as Tower?);
+
+        // Compare the towers position with the troops position and select the empty towers
+        var troops = beliefs.Where(b => b.Item1 is BeliefState.EnemyOnSight or BeliefState.AlliesOnSight)
+            .Select(b => b.Item2 as Troop?).ToList();
+
+        var towerCanGo = towers
+            .Where(tower => troops.All(t => t?.Position != tower?.Position))
+            .DefaultIfEmpty(null)
+            .MinBy(t => Distance(actualTroop,
+                t));
+
+        if (towerCanGo != null)
+            return (IntentionAction.ConquerTower, towerCanGo);
+
+        // Select the action to take for each enemy on sight
+        foreach (var enemy in beliefs.Where(b => b.Item1 is BeliefState.EnemyOnSight)
+                     .Select(b => b.Item2 as Troop?))
+        {
+            if (enemy == null) continue;
+
+            var action = ((actualTroop.Troops + (float)(actualTroop.Troops) * (int)actualTroop.Terrain / 100f) /
+                          (enemy.Value.Troops + enemy.Value.Troops *
+                              ((int)actualTroop.Terrain / 100f))) * (1 / Distance(actualTroop, enemy));
+
+            if (action > AttackProbability)
+                actions.Add((IntentionAction.Attack, enemy, action));
+            else
+            {
+                if (beliefs.Contains((BeliefState.EnemyInRange, enemy)))
+                    actions.Add((IntentionAction.Retreat, enemy, action));
+            }
+        }
+
+        if (actualTroop.Desire == DesireState.GoAhead)
+            return (actions.Count == 0)
+                ? (IntentionAction.Move, null)
+                : actions.OrderByDescending(a => a.Item3).Select(a => (a.Item1, a.Item2)).First();
+
+
+        if (beliefs.Any(b => b.Item1 == BeliefState.EnemyOnSight))
+            return actions.OrderByDescending(a => a.Item3).Select(a => (a.Item1, a.Item2)).First();
+        if (beliefs.Any(b => b.Item1 == BeliefState.AllyTowerOnSight))
+            return (IntentionAction.Wait, beliefs.First(b => b.Item1 == BeliefState.AllyTowerOnSight).Item2);
+        return (IntentionAction.StayClose,
+            beliefs.Where(b => b.Item1 == BeliefState.AllyTowerOnSight)
+                .MinBy(b => Distance(actualTroop, b.Item2 as Tower?)).Item2);
+    }
+
+    public static (IntentionAction, object?) Nlp(string message, Troop actualTroop, IEnumerable<Troop> troops,
+        IEnumerable<Tower> towers)
+    {
         var split = message.Split();
         object? target = null;
         if (split.Length > 1)
         {
-            if (globalAgents.Any(a => a.Name == split[1]))
-                target = globalAgents.First(a => a.Name == split[1]);
-            else
-            {
-                if (globalTowers.Any(t => t.Name == split[1]))
-                    target = globalTowers.First(t => t.Name == split[1]);
-                else
-                    throw new ArgumentException("Name not found");
-            }
+            var targetName = split[1..].Aggregate((a, b) => a + " " + b);
+            target = troops.FirstOrDefault(t => t.Name == targetName);
+            if (target == null)
+                target = towers.FirstOrDefault(t => t.Name == targetName);
+            if (target == null)
+                throw new ArgumentException("Target not found");
         }
 
-        _intention = split[0].ToLower() switch
+        switch (split[0].ToLower())
         {
-            "attack" => (target is Agent) ? (IntentionAction.Attack, target!) : (IntentionAction.AttackTower, target!),
-            "retreat" => (IntentionAction.Retreat, target!),
-            "help" => (IntentionAction.Help, target!),
-            "stayclose" => (IntentionAction.StayClose, target),
-            "move" => (IntentionAction.Move, target),
-            _ => throw new ArgumentException("Command not found")
-        };
-
-        return message;
-    }
-
-    public (IntentionAction, object?) Pipeline(List<Agent> localAgents, List<Agent> globalAgents,
-        List<Tower> localTowers, List<Tower> globalTowers)
-    {
-        Brf(localAgents, globalAgents, localTowers, globalTowers);
-        _followOrder = CheckOrder();
-        if (_followOrder)
-        {
-            _beliefs.Clear();
-            return _intention;
+            case "attack":
+                if(target is Troop troop && troop.Defenders == actualTroop.Defenders)
+                    throw new ArgumentException("Cannot attack an ally");
+                if (target is Tower tower && tower.OccupiedByDefenders == actualTroop.Defenders)
+                    throw new ArgumentException("Cannot attack an ally tower");
+                return target is Troop ? (IntentionAction.Attack, target!) : (IntentionAction.ConquerTower, target!);
+            case "retreat":
+                return (IntentionAction.Retreat, target!);
+            case "stayclose":
+                return (IntentionAction.StayClose, target!);
+            case "wait":
+                return (IntentionAction.Wait, target);
+            case "move":
+                return (IntentionAction.Move, target);
+            default:
+                throw new ArgumentException("Command not found");
         }
-
-        Filter();
-        Execute();
-        _beliefs.Clear();
-        return _intention;
     }
 
-    private bool CheckOrder()
+
+    /// <summary>
+    /// Check if the order is possible given the current state of the world before calling Nlp
+    /// </summary>
+    /// <param name="intentionAction"></param>
+    /// <param name="actualTroop"></param>
+    /// <param name="troops"></param>
+    /// <param name="towers"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public static bool CheckOrder(IntentionAction intentionAction, Troop actualTroop, IEnumerable<Troop> troops,
+        IEnumerable<Tower> towers)
     {
-        return _intention.Item1 switch
+        var beliefs = GetBeliefs(actualTroop, troops, towers);
+        return intentionAction switch
         {
-            IntentionAction.Attack => _beliefs.Any(b => b.Item1 == BeliefState.EnemyOnSight),
-            IntentionAction.AttackTower => _beliefs.Any(b => b.Item1 == BeliefState.TowerOnSight),
-            IntentionAction.Retreat => _beliefs.Any(b => b.Item1 == BeliefState.EnemyOnSight),
-            IntentionAction.Help => _beliefs.Any(b => b.Item1 == BeliefState.AlliesOnSight),
-            IntentionAction.StayClose => _beliefs.Any(b => b.Item1 == BeliefState.Stay),
-            IntentionAction.Move => _beliefs.Any(b => b.Item1 == BeliefState.Move),
+            IntentionAction.Attack => beliefs.Any(b => b.Item1 == BeliefState.EnemyOnSight),
+            IntentionAction.ConquerTower => beliefs.Any(b => b.Item1 == BeliefState.EnemyTowerOnSight),
+            IntentionAction.Retreat => beliefs.Any(b => b.Item1 == BeliefState.EnemyOnSight),
+            IntentionAction.StayClose => beliefs.Any(b => b.Item1 == BeliefState.AllyTowerOnSight),
+            IntentionAction.Move => true,
             _ => throw new ArgumentOutOfRangeException()
         };
     }
-
-    public abstract bool IsAlly(Agent agent);
-    public abstract bool IsAlly(Tower tower);
-}
-
-class Attacker : Agent
-{
-    public Attacker((int, int) position, int troops, DesireState desire, string name) : base(position, troops, desire,
-        name) => Console.WriteLine("Attacker created");
-
-    public override bool IsAlly(Agent agent) => agent is Attacker;
-
-    public override bool IsAlly(Tower tower) => !tower.OccupiedByDefender;
-}
-
-class Defender : Agent
-{
-    public Defender((int, int) position, int troops, DesireState desire, string name) : base(position, troops, desire,
-        name) => Console.WriteLine("Defender created");
-
-    public override bool IsAlly(Agent agent) => agent is Defender;
-
-    public override bool IsAlly(Tower tower) => tower.OccupiedByDefender;
 }
