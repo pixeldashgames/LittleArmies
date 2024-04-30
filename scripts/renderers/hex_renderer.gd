25878b96575eb824dc8642808fb826cd057c7d3e
PixelDG <35879142+leoamaro01@users.noreply.github.com>
2024-04-29 22:45:52 -0400

    Added support for variable shadow cascades

--- scripts/renderers/hex_renderer.gd
+++ scripts/renderers/hex_renderer.gd
@@ -1,11 +1,12 @@
 extends Node3D
 
 @export var chunk_size: int = 8
+@export var shadow_cascades: int = 5
 @export var multi_mesh_scene: PackedScene
 @export var mutli_mesh_shadowed_scene: PackedScene
 
 var normal_mmeshes: Array = []
-var shadowed_mmeshes: Array = []
+var shadowed_mmeshes_levels: Array = []
 
 var vertical_chunks
 var horizontal_chunks
@@ -19,24 +20,40 @@ func render(game_map: GameMap):
 	vertical_chunks = ceili(map_height / chunk_size)
 	horizontal_chunks = ceili(map_width / chunk_size)
 
-	# TODO: Different shadow levels
+	shadowed_mmeshes_levels = []
+	shadowed_mmeshes_levels.resize(shadow_cascades)
 
 	for i in range(vertical_chunks):
 		normal_mmeshes.append([])
-		shadowed_mmeshes.append([])
+
+		for k in range(shadow_cascades):
+			shadowed_mmeshes_levels[k].append([])
+
 		for j in range(horizontal_chunks):
 			var from: Vector2i = Vector2i(j * chunk_size, i * chunk_size)
 			var to: Vector2i = Vector2i(min((j + 1) * chunk_size, map_width), min((i + 1) * chunk_size, map_height))
 
 			var multi_mesh_instance = multi_mesh_scene.instantiate()
 			add_child(multi_mesh_instance)
-			multi_mesh_instance.render(game_map, from, to)
+			multi_mesh_instance.render(game_map, from, to, shadow_cascades, shadow_cascades)
 			normal_mmeshes[i].append(multi_mesh_instance)
 
-			var shadowed_mm = mutli_mesh_shadowed_scene.instantiate()
-			add_child(shadowed_mm)
-			shadowed_mm.render(game_map, from, to)
-			shadowed_mmeshes[i].append(shadowed_mm)
+			for k in range(shadow_cascades):
+				var shadowed_mm = mutli_mesh_shadowed_scene.instantiate()
+				add_child(shadowed_mm)
+				shadowed_mm.render(game_map, from, to, k, shadow_cascades)
+				shadowed_mmeshes_levels[k][i].append(shadowed_mm)
+
+func get_shadow_cascade(visibility_level: float) -> int:
+	for i in range(shadow_cascades):
+		if visibility_level <= (i / shadow_cascades) + 0.0001:
+			return i
+	return shadow_cascades
+
+func _get_mmesh(i: int, j: int, shadow_cascade: int):
+	return normal_mmeshes[i][j] \
+		if shadow_cascade == shadow_cascades \
+		else shadowed_mmeshes_levels[shadow_cascade][i][j]
 
 func change_visibility(visibility_function: Callable):
 	for i in range(vertical_chunks):
@@ -49,17 +66,15 @@ func change_visibility(visibility_function: Callable):
 			var visibility = -1
 			var mixed = false
 
-			var non_shadowed = []
-			var shadowed = []
+			var elements_per_cascade = []
+			for k in range(shadow_cascades + 1):
+				elements_per_cascade.append([])
 
 			for y in range(from.y, to.y):
 				for x in range(from.x, to.x):
-					var rounded_value: int = 0 if visibility_function.call(Vector2i(x, y)) < 0.0001 else 1
+					var rounded_value: int = get_shadow_cascade(visibility_function.call(Vector2i(x, y)))
 
-					if rounded_value == 0:
-						shadowed.append(Vector2i(x, y))
-					else:
-						non_shadowed.append(Vector2i(x, y))
+					elements_per_cascade[rounded_value].append(Vector2i(x, y))
 
 					if mixed:
 						continue
@@ -71,24 +86,22 @@ func change_visibility(visibility_function: Callable):
 						mixed = true
 
 			if not mixed:
-				normal_mmeshes[i][j].visible = visibility == 1
+				for k in range(shadow_cascades + 1):
+					var show_mmesh = visibility == k
 
-				if visibility == 1:
-					normal_mmeshes[i][j].change_all_instances_visibility(true)
-					
-				shadowed_mmeshes[i][j].visible = visibility == 0
+					var mmesh = _get_mmesh(i, j, k)
 
-				if visibility == 0:
-					shadowed_mmeshes[i][j].change_all_instances_visibility(true)
+					mmesh.visible = show_mmesh
+
+					if show_mmesh:
+						mmesh.change_all_instances_visibility(true)
 				continue
 			
-			normal_mmeshes[i][j].visible = true
-			shadowed_mmeshes[i][j].visible = true
-
-			for element in non_shadowed:
-				normal_mmeshes[i][j].change_instance_visibility(element, true)
-				shadowed_mmeshes[i][j].change_instance_visibility(element, false)
 			
-			for element in shadowed:
-				normal_mmeshes[i][j].change_instance_visibility(element, false)
-				shadowed_mmeshes[i][j].change_instance_visibility(element, true)
+			for k in range(shadow_cascades + 1):
+				var mmesh = _get_mmesh(i, j, k)
+				mmesh.visible = true
+
+				for element in elements_per_cascade[k]:
+					for c in range(shadow_cascades + 1):
+						_get_mmesh(i, j, c).change_instance_visibility(element, c == k)
--- scripts/renderers/water_renderer.gd
+++ scripts/renderers/water_renderer.gd
@@ -1,6 +1,9 @@
 extends CellRenderer
 
-func render(game_map: GameMap, from: Vector2i, to: Vector2i):
+@export var normal_water_color: Color
+@export var fully_shadowed_water_color: Color
+
+func render(game_map: GameMap, from: Vector2i, to: Vector2i, cascade: int, total_cascades: int):
 	var water_array = game_map.water_array
 
 	var new_multimesh = MultiMesh.new()
@@ -8,6 +11,12 @@ func render(game_map: GameMap, from: Vector2i, to: Vector2i):
 	new_multimesh.use_colors = true
 	new_multimesh.mesh = multimesh.mesh
 	multimesh = new_multimesh
+
+	# create a new material off the overlay and change the albedo alpha
+	var material = material_override
+	material = material.duplicate()
+	material.albedo_color = fully_shadowed_water_color.lerp(normal_water_color, 1 - cascade / total_cascades)
+	material_override = material
 
 	var transforms = []
 
